9d93a510ec5afd3f875c6eae47b242895e5ff8d9
Clive4216 <cliveesperance4216@gmail.com>
2025-02-21 10:16:07 +0530

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        private const string Username = "Clive";
+        private const string Password = "123";
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            if (model.Username == Username && model.Password == Password)
+            {
+                Session["Username"] = model.Username;
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Username or Password";
+                return View();
+            }
+        }
+
+        public ActionResult Dashboard()
+        {
+            if (Session["Username"] == null)
+                return RedirectToAction("Index");
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
